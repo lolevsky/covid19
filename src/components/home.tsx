4ab357d21f0d148f5fe36136324cf5eea3fc7744
Leonid Olevsky <leonid.olevsky@gmail.com>
2020-04-03 16:24:10 +0700

    Added cookie support

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
+import Cookies from 'universal-cookie';
 import { Container, Dropdown, DropdownButton } from 'react-bootstrap';
+
+import * as cookieName from '../constants/cookiesname';
 
 import { countryEntity } from '../model/country';
 import { covidAPI } from '../api/covidAPI';
@@ -14,13 +17,15 @@ interface State {
     selectedCountry: string
   }
 
+const cookies = new Cookies();
+
 class HomePage extends Component<Props, State>  {
     constructor(props: any) {
         super(props);
         this.state = {
             isFetching: true,
             countries: [],
-            selectedCountry: "Select country"
+            selectedCountry: cookies.get(cookieName.SELECTED_COUNTRY)
         };
     };
 
@@ -28,6 +33,11 @@ class HomePage extends Component<Props, State>  {
         covidAPI.getAllCountries().then((countries) =>
           this.setState({ countries: countries , isFetching: false})
         );
+    }
+
+    handleClick(country: string) {
+        this.setState({selectedCountry: country});
+        cookies.set(cookieName.SELECTED_COUNTRY, country, { path: '/' });
     }
 
     render() {
@@ -39,7 +49,7 @@ class HomePage extends Component<Props, State>  {
                         <DropdownButton id="dropdown-basic-button" title={this.state.selectedCountry} >
                                 {
                                     this.state.countries.map((country: countryEntity) => 
-                                        <Dropdown.Item eventKey={country.country} onClick={()=>this.setState({selectedCountry: country.country})}>{country.country}</Dropdown.Item>
+                                        <Dropdown.Item eventKey={country.country} onClick={() => {this.handleClick(country.country)}}>{country.country}</Dropdown.Item>
                                     )
                                 }
                         </DropdownButton>
