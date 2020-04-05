c12b530725f7ce527bf2003b3f8f23781ecdc8fb
Leonid Olevsky <leonid.olevsky@gmail.com>
2020-04-05 11:14:27 +0700

    Migrated to google map librery

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 
-import GoogleMapReact from 'google-map-react';
-
 import { firebaseAnalytics } from './firebase/firebase';
 import { covidAPI } from '../api/covidAPI';
 import { countryEntity, createEmptyCountryEntity } from '../model/country';
 import { Modal } from 'react-bootstrap';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-export const DataReactComponent = ({countryEntity, onClickHandler} : { onClickHandler: any, countryEntity: countryEntity, lat: number , lng: number} ) => {
-  const greatPlaceStyle = {
-    position: 'absolute' as 'absolute',
-    color: 'red',
-    fontSize: countryEntity.cases/3000 > 12 ? countryEntity.cases/3000 + 'px' : '12px'
-  }
+// export const DataReactComponent = ({countryEntity, onClickHandler} : { onClickHandler: any, countryEntity: countryEntity, lat: number , lng: number} ) => {
+//   const greatPlaceStyle = {
+//     position: 'absolute' as 'absolute',
+//     color: 'red',
+//     fontSize: countryEntity.cases/3000 > 12 ? countryEntity.cases/3000 + 'px' : '12px'
+//   }
 
-  return  <div style={greatPlaceStyle} onClick={() => onClickHandler()}>
-              <b>{countryEntity.cases}</b>
-          </div>;   
-}
+//   return  <div style={greatPlaceStyle} onClick={() => onClickHandler()}>
+//               <b>{countryEntity.cases}</b>
+//           </div>;   
+// }
 
 interface Props {
+  google: any;
 }
 
 interface State {
@@ -57,27 +57,35 @@ class MapPage extends React.Component<Props, State>{
       firebaseAnalytics.logEvent('Map page - alert ' + err)
       alert(err)});
   }
-  
+
   public render() {
     return (
-        <div style={{ height: '94vh', width: '100%' }}>
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: 'AIzaSyDp_Z7LKwj58lrnzVv0twhso6Q5atFZumM' }}
-            defaultCenter={{
-              lat: 25,
-              lng: 0
-            }}
-            defaultZoom={0}>
-            {this.state.isFetchingCountries ? '' : 
-              this.state.countries.filter((country) => country.cases > 1000).map((country: countryEntity) => 
-              <DataReactComponent
-                lat={country.countryInfo.lat}
-                lng={country.countryInfo.long}
-                countryEntity={country}
-                onClickHandler={()=>this.handleShow(country)}/>
-              )
+        <div>
+          <Map 
+            google={this.props.google} 
+            zoom={3}
+            minZoom={3}
+            disableDefaultUI
+            initialCenter={{
+            lat: 25,
+            lng: 0
+            }}>
+               {this.state.isFetchingCountries ? '' : 
+                  this.state.countries.filter((country) => country.cases > 1000).map((country: countryEntity) => 
+                  <Marker 
+                    title={country.cases+''}
+                    name={country.country}
+                    position={{lat: country.countryInfo.lat, lng: country.countryInfo.long}}
+                    onClick={() => this.handleShow(country)}
+                    icon={{
+                      url: "/logo64.png",
+                      anchor: new google.maps.Point(0,0),
+                      scaledSize: new google.maps.Size(32,32)
+                    }}
+                  />
+                  )
             }
-          </GoogleMapReact>
+            </Map>
           <Modal 
             show={this.state.showModal} 
             onHide={()=>this.handleClose()}
@@ -102,5 +110,7 @@ class MapPage extends React.Component<Props, State>{
     );
   }
 }
-  
-export default MapPage;
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDp_Z7LKwj58lrnzVv0twhso6Q5atFZumM'
+  })(MapPage);
