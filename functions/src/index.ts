dca59d601645f4efff0c2fc157aac1c1333a9f1d
Leonid Olevsky <leonid.olevsky@gmail.com>
2020-04-05 19:42:56 +0700

    Playing with functons

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,22 @@
 import * as functions from 'firebase-functions';
+const fetch = require("node-fetch");
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+export const helloWorld = functions.https.onRequest((request, resp) => {
+    fetch('https://corona.lmao.ninja/countries?sort=country')
+    .then((response: { status: number; statusText: string | undefined; }) => {
+                            if (response.status >= 200 && response.status < 300) {
+                                return response;
+                            } else {
+                                let error = new Error(response.statusText);
+                                throw error;
+                            }})
+    .then((response: { json: () => any; }) => {
+        resp.send(response.json())
+    })
+    .catch((err: Error) => {
+        console.error(err)
+        resp.send('My error ' + err)  
+    })
 });
+
+
