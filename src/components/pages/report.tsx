4faf3c99afb7314f047e2cee5363b2a122a68b76
Leonid Olevsky <leonid.olevsky@gmail.com>
2020-04-05 14:55:48 +0700

    added searchible countries

--- src/components/pages/report.tsx
+++ src/components/pages/report.tsx
@@ -1,7 +1,7 @@
 import React,  { Component } from 'react';
 
 import Cookies from 'universal-cookie';
-import { Container, Dropdown, DropdownButton } from 'react-bootstrap';
+import { Container, Dropdown } from 'react-bootstrap';
 import {
     LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, AreaChart, Area, Bar, BarChart,
   } from 'recharts';
@@ -13,6 +13,7 @@ import { historicalEntity, createEmptyHistoricalEntity } from '../../model/histo
 import { timelineData, createTimelineData } from '../../model/timelineData';
 import { covidAPI } from '../../api/covidAPI';
 import { firebaseAnalytics } from '../firebase/firebase';
+import { CustomMenu } from '../custom/CustomMenu';
 
 interface Props {
 }
@@ -106,81 +107,87 @@ class ReportPage extends Component<Props, State> {
         return (
             <Container>
                 <br/>
-                <div style={{display: 'flex', justifyContent: 'center'}}>{this.state.isFetchingCountries ? 'Fetching data...' : 
+                <div >{this.state.isFetchingCountries ? 
+                                                    <div style={{display: 'flex', justifyContent: 'center'}}>
+                                                    'Fetching data...' 
+                                                    </div>: 
                     <div>
-                        <Container style={{display: 'flex', justifyContent: 'center'}}>
-                                <DropdownButton id="dropdown-basic-button" title={!!this.state.selectedCountryEntity? this.state.selectedCountryEntity.country : 'Please select country'} >
-                                        {
-                                            this.state.countries.map((country: countryEntity) => 
-                                                <Dropdown.Item eventKey={country.country} onClick={() => {this.handleCountryClick(country)}}>{country.country}</Dropdown.Item>
-                                            )
-                                        }
-                                </DropdownButton>
-                            </Container>
+                        <div style={{display: 'flex', justifyContent: 'center'}}>
+                            <Dropdown>
+                                <Dropdown.Toggle  id="dropdown-custom-components">
+                                    {!!this.state.selectedCountryEntity? this.state.selectedCountryEntity.country : 'Please select country'}
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu as={CustomMenu}>
+                                    {
+                                        this.state.countries.map((country: countryEntity) => 
+                                            <Dropdown.Item eventKey={country.country} onClick={() => {this.handleCountryClick(country)}}>{country.country}</Dropdown.Item>
+                                        )
+                                    }
+                                </Dropdown.Menu>
+                             </Dropdown>
+                        </div>
+                        <div style={{display: 'flex', justifyContent: 'center'}}>
+                        {
+                            !!this.state.selectedCountryEntity?
+                                <div style={{display: 'flex', justifyContent: 'center'}}>
+                                <p>
+                                Today In <b>{
+                                this.state.selectedCountryEntity.country
+                                }</b> are <b>{
+                                this.state.selectedCountryEntity.cases
+                                }</b> cases, <b>{
+                                this.state.selectedCountryEntity.recovered
+                                }</b> recovered and <b>{
+                                this.state.selectedCountryEntity.deaths
+                                }</b> deaths
+                                </p>    
+                                </div>
+                                : ''
+                        }
+                        </div>
                         {this.state.isFetchingData ? '' : 
-                            <Container>
+                            <div>
+                                <ResponsiveContainer width='100%' aspect={4.0/3.0}>
+                                    <LineChart
+                                        data={this.state.timelineData}>
+                                        <CartesianGrid strokeDasharray="3 3" />
+                                        <XAxis dataKey="date" tick={{fontSize: 10}}/>
+                                        <YAxis />
+                                        <Tooltip />
+                                        <Legend />
+                                        <Line type="monotone" dataKey="cases" stroke="#8884d8" activeDot={{ r: 8 }} />
+                                        <Line type="monotone" dataKey="deaths" stroke="#82ca9d" />
+                                        <Line type="monotone" dataKey="recovered" stroke="#ffc658" />
+                                    </LineChart>
+                                </ResponsiveContainer>
                                 <br/>
-                                <div style={{display: 'flex', justifyContent: 'center'}}>
-                                    <p>
-                                        Today In <b>{
-                                        this.state.selectedCountryEntity.country
-                                        }</b> are <b>{
-                                        this.state.selectedCountryEntity.cases
-                                        }</b> cases, <b>{
-                                        this.state.selectedCountryEntity.recovered
-                                        }</b> recovered and <b>{
-                                        this.state.selectedCountryEntity.deaths
-                                        }</b> deaths
-                                    </p>    
-                                </div>
+                                <ResponsiveContainer width='100%' aspect={4.0/3.0}>
+                                    <BarChart data={this.state.timelineData}>
+                                        <CartesianGrid strokeDasharray="3 3" />
+                                        <XAxis dataKey="date" tick={{fontSize: 10}}/>
+                                        <YAxis />
+                                        <Tooltip />
+                                        <Legend />
+                                        <Bar dataKey="cases" fill="#8884d8" />
+                                        <Bar dataKey="deaths" fill="#82ca9d" />
+                                        <Bar dataKey="recovered" fill="#ffc658" />
+                                    </BarChart>
+                                </ResponsiveContainer>
                                 <br/>
-                                <div style={{display: 'flex', justifyContent: 'center'}}>
-                                    <ResponsiveContainer width='100%' aspect={4.0/3.0}>
-                                        <LineChart
-                                            data={this.state.timelineData}>
-                                            <CartesianGrid strokeDasharray="3 3" />
-                                            <XAxis dataKey="date" tick={{fontSize: 10}}/>
-                                            <YAxis />
-                                            <Tooltip />
-                                            <Legend />
-                                            <Line type="monotone" dataKey="cases" stroke="#8884d8" activeDot={{ r: 8 }} />
-                                            <Line type="monotone" dataKey="deaths" stroke="#82ca9d" />
-                                            <Line type="monotone" dataKey="recovered" stroke="#ffc658" />
-                                        </LineChart>
-                                    </ResponsiveContainer>
-                                </div>
-                                <br/>
-                                <div style={{display: 'flex', justifyContent: 'center'}}>
-                                    <ResponsiveContainer width='100%' aspect={4.0/3.0}>
-                                        <BarChart data={this.state.timelineData}>
-                                            <CartesianGrid strokeDasharray="3 3" />
-                                            <XAxis dataKey="date" tick={{fontSize: 10}}/>
-                                            <YAxis />
-                                            <Tooltip />
-                                            <Legend />
-                                            <Bar dataKey="cases" fill="#8884d8" />
-                                            <Bar dataKey="deaths" fill="#82ca9d" />
-                                            <Bar dataKey="recovered" fill="#ffc658" />
-                                        </BarChart>
-                                    </ResponsiveContainer>
-                                </div>
-                                <br/>
-                                <div style={{display: 'flex', justifyContent: 'center'}}>
-                                    <ResponsiveContainer width='100%' aspect={4.0/3.0}>
-                                        <AreaChart width={500}
-                                            height={400}
-                                            data={this.state.timelineData}>
-                                            <CartesianGrid strokeDasharray="3 3" />
-                                            <XAxis dataKey="date" tick={{fontSize: 10}}/>
-                                            <YAxis />
-                                            <Tooltip />
-                                            <Area type="monotone" dataKey="cases" stackId="1" stroke="#8884d8" fill="#8884d8" />
-                                            <Area type="monotone" dataKey="deaths" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
-                                            <Area type="monotone" dataKey="recovered" stackId="1" stroke="#ffc658" fill="#ffc658" />
-                                        </AreaChart>
-                                    </ResponsiveContainer>
-                                </div>
-                            </Container>
+                                <ResponsiveContainer width='100%' aspect={4.0/3.0}>
+                                    <AreaChart width={500}
+                                        height={400}
+                                        data={this.state.timelineData}>
+                                        <CartesianGrid strokeDasharray="3 3" />
+                                        <XAxis dataKey="date" tick={{fontSize: 10}}/>
+                                        <YAxis />
+                                        <Tooltip />
+                                        <Area type="monotone" dataKey="cases" stackId="1" stroke="#8884d8" fill="#8884d8" />
+                                        <Area type="monotone" dataKey="deaths" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+                                        <Area type="monotone" dataKey="recovered" stackId="1" stroke="#ffc658" fill="#ffc658" />
+                                    </AreaChart>
+                                </ResponsiveContainer>
+                            </div>
                         }
                     </div>
                 }</div>
